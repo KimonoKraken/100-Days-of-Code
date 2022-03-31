a09ab059dae2d887393c0a4717badc0943dfd686
Dimitriy K <45632694+KimonoKraken@users.noreply.github.com>
2022-03-31 08:51:06 -0700

    Update main.py

--- Intermediate Projects/Snake Game/main.py
+++ Intermediate Projects/Snake Game/main.py
@@ -27,23 +27,24 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
+    scoreboard.update_scoreboard()
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.clear()
         scoreboard.increase_score()
 
-    # Detect collision wilt wall.
+    # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
