2b4da1f39ecc24c62a052dbfa352a70c3e459575
Dimitriy K <45632694+KimonoKraken@users.noreply.github.com>
2022-04-12 16:34:09 -0700

    Update main.py

--- Intermediate Projects/Password Manager/main.py
+++ Intermediate Projects/Password Manager/main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -33,17 +33,48 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+                    "email": email,
+                    "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title="Oops!", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n\nEmail: {email}"
-                                                  f"\nPassword: {password} \n\nIs it OK to save?")
-        if is_ok:
-            with open("password_vault.txt", "a") as data_file:
-                data_file.write(f" {website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("password_vault.json", "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            # creating a new json file to write user entries to
+            with open("password_vault.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # updating old data with new data
+            data.update(new_data)
+
+            with open("password_vault.json", "w") as data_file:
+                # saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- SEARCH FOR PASSWORD ------------------------------- #
+
+
+def search():
+    website = website_entry.get()
+    with open("password_vault.json", "r") as data_file:
+        # reading old data
+        data = json.load(data_file)
+        for site in data:
+            if website == site:
+                messagebox.showinfo(title=site, message=f"Email: {data[site]['email']}\n"
+                                                        f"Password: {data[site]['password']}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -68,11 +99,11 @@ password_label = Label(text="Password:", bg="white")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=45)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=1)
 website_entry.focus()
-email_entry = Entry(width=45)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=1)
 email_entry.insert(0, "dkmiamib@gmail.com")
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
@@ -82,8 +113,8 @@ password_button = Button(text="Generate", command=generate_pass, bg="white")
 password_button.grid(row=3, column=2, columnspan=1)
 add_button = Button(text="Add", command=save, width=38, bg="white")
 add_button.grid(row=4, column=1, columnspan=2)
-
-
+search_button = Button(text="Search", command=search, bg="white")
+search_button.grid(row=1, column=2, columnspan=1)
 
 
 
